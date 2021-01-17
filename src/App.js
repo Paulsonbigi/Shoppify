4c4fa5d9c83a24fb1336e236e0ef66ddd7389e79
Paulsonbigi <adegbokanpaul@gmail.com>
2021-01-17 19:05:25 +0100

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, Fragment } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import axios from "axios"
-import { Container, Row, Spinner, Alert} from "reactstrap"
+import { Container, Row, Spinner, Alert, UncontrolledAlert } from "reactstrap"
 import MovieList from "./components/MovieList"
 import InputSearch from "./components/InputSearch"
 import NomineeComp from "./components/AddToList"
@@ -14,6 +14,9 @@ const App =() => {
   const [ search, setSearch] = useState("")
   const [errMsg, setErrMsg] = useState(null)
   const [nominated, setNominated] = useState([])
+  const [visible, setVisible] = useState(true)
+
+  const onDismiss = () => setVisible(false)
 
   const MovieRequest = async(search) => {
     setLoading(true)
@@ -24,6 +27,7 @@ const App =() => {
       .then(res => {
         const result = res.data
 
+        
         if(result.Search){
           setMovies(result.Search)
           setLoading(false)
@@ -34,23 +38,33 @@ const App =() => {
 
   useEffect(() => {
     MovieRequest(search);  
+    console.log(nominated)
     
   }, [search])
 
   const movieNominated = (movie) => {
 
-    const newMovies = [...nominated, movie]
-    if(nominated.length < 5){
-        setNominated(newMovies)
+    const alreadyNominated = nominated.map(one => one.Title)
+    if(!alreadyNominated.includes(movie.Title)){
+      const newMovies = [...nominated, movie]
+      if(newMovies.length < 6) {
+        return setNominated(newMovies)
+      }
+      
     }
     else{
-      return maxNomination
+      setErrMsg({ errMsg: "Movie already nominated"})
+      console.log("Movied already nominated")
     }
+    
     
   }
 
   const maxNomination = (
-    <Alert color="primary">Maximum nomination is 5!</Alert>
+    <UncontrolledAlert color="primary">Maximum nomination is 5!</UncontrolledAlert>
+  )
+  const alreadyExists = (
+    <Alert color="danger" isOpen={visible} toggle={onDismiss}>Movie already nominated</Alert>
   )
 
   const removeNominee = (movie) => {
@@ -65,6 +79,7 @@ const App =() => {
       <div className='justify-content-md-center mt-6 mb-4'>
 				<h4 className="mb-4 mt-4 brand">The Shoppies</h4>
         { nominated.length >=5 ? maxNomination : ""}
+        { errMsg ? alreadyExists : ""}
 				<InputSearch search={search} setSearch={setSearch} />
 			</div>
       <Fragment>
